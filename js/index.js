89f9d97fb8ba822f8c09b81bc20986219ac84e28
Connie Tang <kourtneytg@gmail.com>
2020-11-12 17:19:02 +1300

    updated topnav bar
    make topnav bar become smaller while scrolling up

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     topnavChange();
+
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {//scroll up effective -> become smaller
+        // $(".topnav")
+        $("#logo").css("height", "60px");
+        $(".logoDiv").css("display", "inline-block");
+        $(".topnav .menu-content").css("margin-top", "0");
+        $(".topnav .menu-content").css("display", "inline-block");
+    }
+    else {
+        $("#logo").css("height", "200px");
+        $(".logoDiv").css("display", "block");
+        // $(".logoDiv").css("vertical-align", "middle");
+        // $(".logoDiv").css("text-align", "center");
+
+        $(".topnav .menu-content").css("margin-top", "10px");
+        $(".topnav .menu-content").css("display", "block");
+        // $(".topnav .menu-content").css("vertical-align", "middle");
+        // $(".topnav .menu-content").css("text-align", "center");
+        
+    }
 }
+
 function topnavChange(){
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav responsive") {
