6f807a466f912a428f9a1eac2ddac9162fe64f30
John Du <gzdhy8895@gmail.com>
2021-01-20 12:24:32 +1300

    refactor file structure (layout)

---
+++ _theme/default/js/init.js
@@ -0,0 +1,47 @@
+$.getJSON("_data/site_setup/config.json", function (json) {
+    const THEME = json.theme;
+
+    $.get(`_theme/${THEME}/common/annoucement.html`,
+        function (data, status) {
+            document.getElementById("annoucement").innerHTML = data;
+            var myIndex = 0;
+            annoucementBanner(myIndex);
+        }
+    );
+
+    $.get(`_theme/${THEME}/common/nav.php`,
+        function (data, status) {
+            document.getElementById("myTopnav").innerHTML = data;
+        }
+    );
+
+    $.get("dynamic_pages/footer-list.html",
+        function (data, status) {
+            document.getElementById("footer-list").innerHTML = data;
+        }
+    );
+
+    $.get(`_theme/${THEME}/common/footer.html`,
+        function (data, status) {
+            document.getElementById("footer").innerHTML = data;
+        }
+    );
+
+    function annoucementBanner(myIndex) {
+        var i;
+        var x = document.getElementsByClassName("annoucement");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1
+        }
+        x[myIndex - 1].style.display = "block";
+
+        setTimeout(function () {
+            annoucementBanner(myIndex);
+        }, 4000);
+    }
+});
+
--- js/init.js
+++ js/init.js
@@ -1,42 +1,9 @@
-$.get("layout/annoucement.html",
-    function(data, status) {
-        document.getElementById("annoucement").innerHTML = data;
-        var myIndex = 0;
-        annoucementBanner(myIndex);
-    }
-);
+$.getJSON("_data/site_setup/config.json", function (json) {
+    const THEME = json.theme;
+    console.log(THEME); // show the JSON file content into console
 
-$.get("layout/nav.php",
-    function(data, status) {
-        document.getElementById("myTopnav").innerHTML = data;
-    }
-);
 
-$.get("dynamic_pages/footer-list.html",
-    function(data, status) {
-        document.getElementById("footer-list").innerHTML = data;
-    }
-);
-
-$.get("layout/footer.html",
-    function(data, status) {
-        document.getElementById("footer").innerHTML = data;
-    }
-);
-
-function annoucementBanner(myIndex) {
-    var i;
-    var x = document.getElementsByClassName("annoucement");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1
-    }
-    x[myIndex - 1].style.display = "block";
-
-    setTimeout(function() {
-        annoucementBanner(myIndex);
-    }, 4000);
-}
+    var my_awesome_script = document.createElement('script');
+    my_awesome_script.setAttribute('src', `_theme/${THEME}/js/init.js`);
+    document.head.appendChild(my_awesome_script);
+});
